e5a26143b2fca70035da3166332c7fc20cedda79
Rodrigo Francisco <rodrigofrancisco@tecnico.ulisboa.pt>
2025-01-30 19:46:57 +0000

    created utils module for random useful functions

---
+++ src/utils/colored_text.py
@@ -0,0 +1,13 @@
+
+
+def red(text: str) -> str:
+    return f"\033[91m{text}\033[0m"
+
+def green(text: str) -> str:
+    return f"\033[92m{text}\033[0m"
+
+def yellow(text: str) -> str:
+    return f"\033[93m{text}\033[0m"
+
+def blue(text: str) -> str:
+    return f"\033[94m{text}\033[0m"
