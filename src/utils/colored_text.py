06a4f6615c0a83846481e202a100342659d1b4dd
Rodrigo Francisco <rodrigofrancisco@tecnico.ulisboa.pt>
2025-03-26 12:01:36 +0000

    added a flag to turn off all colors

--- src/utils/colored_text.py
+++ src/utils/colored_text.py
@@ -1,13 +1,13 @@
-
+COLORED_TEXT = True
 
 def red(text: str) -> str:
-    return f"\033[91m{text}\033[0m"
+    return f"\033[91m{text}\033[0m" if COLORED_TEXT else text
 
 def green(text: str) -> str:
-    return f"\033[92m{text}\033[0m"
+    return f"\033[92m{text}\033[0m" if COLORED_TEXT else text
 
 def yellow(text: str) -> str:
-    return f"\033[93m{text}\033[0m"
+    return f"\033[93m{text}\033[0m" if COLORED_TEXT else text
 
 def blue(text: str) -> str:
-    return f"\033[94m{text}\033[0m"
+    return f"\033[94m{text}\033[0m" if COLORED_TEXT else text
