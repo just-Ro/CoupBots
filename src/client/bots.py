380c1ccd9941ea574767c0b3bf65c8d325eeed91
Rodrigo Francisco <rodrigofrancisco@tecnico.ulisboa.pt>
2025-05-30 17:47:38 +0100

    updated docs

--- src/client/bots.py
+++ src/client/bots.py
@@ -23,7 +23,7 @@ class CoupBot(InformedPlayer):
         msg (GameMessage): Message to be sent to the server.
         players (dict[str, PlayerSim]): Dictionary of players in the game.
         possible_messages (list[str]): List of possible messages the player can send.
-        rcv_msg (GameMessage): Last received message.
+        history (list[GameMessage]): History of received messages.
         ready (bool): Flag for whether the player is ready or not.
         replied (bool): Flag for whether the player has replied to the last message or not.
         state (PlayerState): State of the player.
